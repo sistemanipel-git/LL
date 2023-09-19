ab5ebff52f350534f59e7160dca2139d156ea9f3
sistemanipel-git <zeleznov.sergey@gmail.com>
2023-09-19 21:53:44 +0100

    homework_2

---
+++ js-variables/variables.js
@@ -0,0 +1,14 @@
+let userName = "Jesus";
+console.log(typeof userName);
+
+let adminName = 12.5;
+console.log(typeof adminName);
+
+let dohomework_1 = "awesome";
+let dohomework_2 = "programming language";
+let dohomework_3 = "test automation script";
+console.log(`Javascript is: ${dohomework_1} ${dohomework_3} ${dohomework_2}`);
+
+let my_mood = "sorrow";
+my_mood = "hilarious";
+console.log(my_mood);
